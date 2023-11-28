5a9fa5c954e11f685c4573383d27f436c5931810
Yuto <agbt.itohyuto@gmail.com>
2023-11-28 15:46:42 +0900

    メモアプリを新規追加。
    - 開発環境と本番環境を分けるように実装。
    - ページのヘッダー部分はlayout.erbでまとめる。
    - 存在しないURLへのアクセス時と存在しないメモを指定したときのエラーハンドリングを実装。
    - メモのデータは/data/memo.csvで管理する。
    - XSS対策。

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # note-taking-application
+## 前提条件
+- rubyのバージョンが3.2.2が使用できること。
+  ```
+  rbenv versions
+  ```
+- bundlerがインストールされていること。
+  下記のコマンドでバージョンが表示される事を確認してください。
+  ```
+  bundler -v
+  ```
+## 使い方
+1. このリポジトリのクローンを任意のディレクトリに作成します。
+  ここでは、`$HOME/ruby/note-taking-application`を使用します。
+  ```
+  mkdir -p $HOME/ruby/note-taking-application
+  git clone https://github.com/ItohYuto/note-taking-application.git "$HOME/ruby/note-taking-application"
+  ```
+2. カレントディレクトリをクローンを作成したディレクトリに移動します。
+```
+cd $HOME/ruby/note-taking-application
+```
+3. bundlerで必要なgemをインストールする。
+```
+bundle install
+```
+※本番環境でのみ使用する場合は下記コマンドでgemをインストールしてください。
+この方法でインストールした場合、アプリケーションの起動時に開発環境での起動ができないため、ご注意ください。
+```
+bundle config set --local without 'development'
+bundle install
+```
+4. アプリケーションの起動
+- 開発環境(コード変更した際にオートリロードができます。)
+```
+bundle exec rackup config.ru
+```
+- 本番環境
+```
+APP_ENV=production bundle exec rackup config.ru
+```
+5. ブラウザで下記のURLにアクセスする。
+```
+http://localhost:9292/
+```
---
+++ app.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'csv'
+
+APP_NAME = 'メモアプリ'
+CSV_HEADER = %w[id title content].freeze
+
+class NoteTakingApplication < Sinatra::Base
+  configure do
+    enable :method_override
+    set :root, File.join(File.dirname(__FILE__), '')
+    if Dir.children("#{root}data").none? { |file| file == 'memos.csv' }
+      CSV.open("#{root}data/memos.csv", 'w') do |csv|
+        csv << CSV_HEADER
+      end
+    end
+  end
+
+  configure :development do
+    require 'sinatra/reloader'
+    register Sinatra::Reloader
+  end
+
+  helpers do
+    def data_file_path
+      File.join(settings.root, 'data/memos.csv')
+    end
+
+    def read_notes
+      CSV.table(data_file_path, headers: true)
+    end
+
+    def h(text)
+      Rack::Utils.escape_html(text)
+    end
+  end
+
+  before do
+    @title = APP_NAME
+    @notes = read_notes
+  end
+
+  not_found do
+    redirect '/not_found'
+  end
+
+  get '/not_found' do
+    @sub_title = 'ページが存在しません。'
+    erb :not_found
+  end
+
+  get '/notes/not_found' do
+    @sub_title = 'メモが存在しません。'
+    erb :note_not_found
+  end
+
+  get '/' do
+    @sub_title = '一覧'
+    erb :list
+  end
+
+  get '/register' do
+    @sub_title = '新規登録画面'
+    erb :register
+  end
+
+  get '/notes/:note_id' do |note_id|
+    redirect '/notes/not_found' if @notes.none? { |note| note[:id] == note_id.to_i }
+    @notes.each do |note|
+      if note[:id] == note_id.to_i
+        @sub_title = note[:title]
+        @note = note
+      end
+      next if note[:id] == note_id.to_i
+    end
+    erb :view
+  end
+
+  get '/notes/:note_id/edit' do |note_id|
+    redirect '/notes/not_found' if @notes.none? { |note| note[:id] == note_id.to_i }
+    @notes.each do |note|
+      if note[:id] == note_id.to_i
+        @sub_title = "#{note[:title]}の編集画面"
+        @note = note
+      end
+      next if note[:id] == note_id.to_i
+    end
+    erb :edit
+  end
+
+  post '/notes' do
+    redirect '/notes/not_found' if @notes.none? { |note| note[:id] == note_id.to_i }
+    note_id = if @notes.empty?
+                1
+              else
+                @notes.max_by { |note| note[:id] }[:id] + 1
+              end
+    CSV.open(data_file_path, 'a') do |csv|
+      csv << [note_id.to_i, h(params[:title]), h(params[:content])]
+    end
+    redirect '/'
+  end
+
+  patch '/notes/:note_id' do |note_id|
+    redirect '/notes/not_found' if @notes.none? { |note| note[:id] == note_id.to_i }
+    edit_note = (@notes.delete_if { |note| note[:id] == note_id.to_i } << [note_id.to_i, h(params[:title]), h(params[:content])]).sort_by { |note| note[:id] }
+    CSV.open(data_file_path, 'w', headers: true) do |csv|
+      csv << CSV_HEADER
+      edit_note.each { |note| csv << note }
+    end
+    redirect '/'
+  end
+
+  delete '/notes/:note_id' do |note_id|
+    redirect '/notes/not_found' if @notes.none? { |note| note[:id] == note_id.to_i }
+    deleted_notes = @notes.delete_if { |note| note[:id] == note_id.to_i }
+    CSV.open(data_file_path, 'w', headers: true) do |csv|
+      csv << CSV_HEADER
+      deleted_notes.each { |note| csv << note }
+    end
+    redirect '/'
+  end
+end
